78b23b23ac96c1fb7841610e8b9c1c18856b9ceb
Marwan Zaky <marouaneezzaky@gmail.com>
2023-01-24 16:06:37 +0100

    Update mongodb-import/import.py

--- mongodb-import/import.py
+++ mongodb-import/import.py
@@ -5,15 +5,36 @@ import json
 from dotenv import load_dotenv
 load_dotenv('mongodb-import/.env')
 
-MONGO_DB_PASSWORD = os.environ.get("MONGO_DB_PASSWORD")
-MONGO_DB_DATABASE = os.environ.get("MONGO_DB_DATABASE").replace(
-    "<password>", MONGO_DB_PASSWORD)
+
+def Average(lst):
+    return sum(lst) / len(lst)
+
+
+MONGO_DB_PASSWORD = os.environ.get('MONGO_DB_PASSWORD')
+MONGO_DB_DATABASE = os.environ.get('MONGO_DB_DATABASE').replace(
+    '<password>', MONGO_DB_PASSWORD)
 
 myclient = pymongo.MongoClient(MONGO_DB_DATABASE)
-mydb = myclient["ecommerce"]
-myproducts = mydb["products"]
+mydb = myclient['ecommerce']
 
-f = open("mongodb-import/products.json")
-myproducts_data = json.load(f)
+myproducts = mydb['products']
+myreviews = mydb['reviews']
+myusers = mydb['users']
+
+myproducts_data = json.load(open('mongodb-import/products.json'))
+myreviews_data = json.load(open('mongodb-import/reviews.json'))
+myusers_data = json.load(open('mongodb-import/users.json'))
+
+for myproduct in myproducts_data:
+    ratings = []
+
+    for myreview in myreviews_data:
+        if myreview['product'] == myproduct['_id']:
+            ratings.append(myreview['rating'])
+
+    myproduct['avgRatings'] = Average(ratings)
+    myproduct['numReviews'] = len(ratings)
 
 x = myproducts.insert_many(myproducts_data)
+y = myreviews.insert_many(myreviews_data)
+z = myusers.insert_many(myusers_data)
